824e639a8afe97bc49da658c2311f75d30303113
Yamini-Kanuru <yamini22110215@snuchennai.edu.in>
2025-11-11 15:42:27 +0530

    presentation

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Yamini Krishna Kanuru’s CI/CD Pipeline running on AWS!');
+  res.send('hi Dhivya maam!');
 });
 
 const PORT = process.env.PORT || 3000;
